e0b8d5d006171cac455b2b27763fec0c76bffd61
payatsu <6873447+payatsu@users.noreply.github.com>
2021-06-30 00:31:14 +0900

    update scripts/modify-config.sh

--- scripts/modify-config.sh
+++ scripts/modify-config.sh
@@ -14,3 +14,31 @@ config \
 	-E KGDB KGDB_SERIAL_CONSOLE \
 	-E KGDB KGDB_KDB \
 	|| exit
+
+config \
+	-e PROFILING \
+	-e HAVE_PERF_EVENTS \
+	-e PERF_EVENTS \
+	|| exit
+
+config \
+	-e HAVE_KPROBES \
+	-e KPROBES \
+	-e HAVE_REGS_AND_STACK_ACCESS_API \
+	-e KPROBE_EVENTS \
+	-e ARCH_SUPPORTS_UPROBES \
+	-e MMU \
+	-e UPROBES \
+	-e UPROBE_EVENTS \
+	|| exit
+
+config \
+	-e BPF \
+	-e BPF_SYSCALL \
+	-e HAVE_EBPF_JIT \
+	-e BPF_JIT \
+	-e BPF_EVENTS \
+	-e IKHEADERS \
+	-m NET_CLS_BPF \
+	-m NET_ACT_BPF \
+	|| exit
